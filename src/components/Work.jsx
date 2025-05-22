b21728cd3eaac7dcdb28016cdae0171d1989a9ea
seiyanz16 <zaskiafitrisholehah@gmail.com>
2025-05-22 19:04:26 +0700

    refactor: change work section text

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -31,7 +31,7 @@ const Work = () => {
   return (
     <section id="work" className="section">
       <div className="container">
-        <h2 className="headline-2 mb-8 reveal-up">My portfolio highlights</h2>
+        <h2 className="headline-2 mb-8 reveal-up">My Portfolio Highlights</h2>
 
         <div className="grid gap-x-4 gap-y-5 grid-cols-[repeat(auto-fill,_minmax(280px,_1fr))]">
           {works.map(({ imgSrc, title, description, tags, projectLink }, key) => (
